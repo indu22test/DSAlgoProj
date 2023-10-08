ff728da648a8e9fed6cf5fe2045f6bd42a0b0467
indu2 <indu2@Saanvi_Singh>
2023-10-08 00:49:07 -0400

    Commit project

---
+++ DSAlgoProj/src/test/java/stepDefinitions/BaseClass.java
@@ -0,0 +1,12 @@
+package stepDefinitions;
+
+import pageObjects.HomePage;
+
+import pageObjects.LoginPage;
+import pageObjects.RegisterPage;
+
+public class BaseClass {
+ public HomePage HomePage;
+ public RegisterPage RegisterPage;
+ public LoginPage LoginPage;
+}
---
+++ DSAlgoProj/src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,22 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+
+plugin={"pretty", "html:target/MyReports/report.html",
+		"junit:target/MyReports/report.junit"},
+monochrome=false,		
+tags ="@registration",
+features= {"src/test/resource/features"},
+glue= "stepDefinitions")
+
+public class TestRunner {
+
+}
